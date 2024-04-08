6efa12b5d0d117626f407b51189692f99b929c35
Vince Delgado <116175769+VKD07@users.noreply.github.com>
2024-04-08 15:48:05 +0400

    Polished Car Manual

--- Delivery/Assets/Scripts/Navigator/CarManualInitializer.cs
+++ Delivery/Assets/Scripts/Navigator/CarManualInitializer.cs
@@ -1,51 +1,121 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CarManualInitializer : MonoBehaviour
 {
-    [SerializeField] Transform troubleShootPanel;
+    [SerializeField] Transform panelParent;
+    [Header("=== UI PREFABS ===")]
+    [SerializeField] GameObject troubleShootPanel;
     [SerializeField] GameObject troubleShootPrefab;
     [SerializeField] CarTroubleShooting[] carTroubleShootings;
     Sprite prevImage;
     TextMeshProUGUI prevText;
+
+
     GameObject troubleShootUI;
+
+    [Header("=== LIST OF PANELS ===")]
+    [SerializeField] List<GameObject> troubleShootPanels;
+
+
     bool textIsUpdated;
+    int currentSpawnedContent;
+    bool firstIndex;
+    GameObject panel;
+    bool newPanel;
     void Awake()
     {
-        InitTroubleShootUI();
+        //InitTroubleShootUI();
+        StartCoroutine(InitTroubleShootPanels());
     }
 
-    void InitTroubleShootUI()
+    IEnumerator InitTroubleShootPanels()
     {
-        for (int i = 0; i < carTroubleShootings.Length; i++)
+        while (currentSpawnedContent < carTroubleShootings.Length)
         {
-            //If current loop symbol is equal to prev image then dont instantiate
-            textIsUpdated = false;
-            if (carTroubleShootings[i].getSpriteSymbol != prevImage)
-            {
-                troubleShootUI = Instantiate(troubleShootPrefab, troubleShootPanel.position, Quaternion.identity);
-                troubleShootUI.transform.parent = troubleShootPanel.transform;
+            yield return null;
 
-                Image symbol = troubleShootUI.transform.Find("CarSymbol_Img").GetComponent<Image>();
-                prevImage = carTroubleShootings[i].getSpriteSymbol;
-                symbol.sprite = carTroubleShootings[i].getSpriteSymbol;
-            }
-            else
+            if (!newPanel)
             {
-                TextMeshProUGUI textMesh = troubleShootUI.GetComponentInChildren<TextMeshProUGUI>();
-                prevText = textMesh;
-                prevText.text += $"\n {SetInstructionTxt(carTroubleShootings[i].GetSmokeColorEnum, carTroubleShootings[i].GetKeyCodes, carTroubleShootings[i].timeToHold)}";
-                textIsUpdated = true;
+                panel = Instantiate(troubleShootPanel, panelParent.position, Quaternion.identity);
+                panel.transform.parent = panelParent;
+                troubleShootPanels.Add(panel);
+                panel.SetActive(false);
+                newPanel = true;
             }
-            
-            if(!textIsUpdated)
+
+            //Make sure first panel is always enabled
+            if (!firstIndex)
             {
-                TextMeshProUGUI textMesh = troubleShootUI.GetComponentInChildren<TextMeshProUGUI>();
-                textMesh.text = SetInstructionTxt(carTroubleShootings[i].GetSmokeColorEnum, carTroubleShootings[i].GetKeyCodes, carTroubleShootings[i].timeToHold);
-                prevText = textMesh;
+                firstIndex = true;
+                panel.SetActive(true);
+            }
+
+            InitTroubleShootUI(currentSpawnedContent, panel.transform);
+
+            if (currentSpawnedContent < carTroubleShootings.Length)
+            {
+                currentSpawnedContent++;
+            }
+
+            //Make sure theres only 3 contents in one panel, else make a new panel
+            if (panel.transform.childCount == 3)
+            {
+                newPanel = false;
+            }
+            else if (panel.transform.childCount <= 0)
+            {
+                newPanel = false;
+                Destroy(panel);
+                panel = null;
+            }
+        }
+
+        Invoke("RemoveAllEmptyObjectsFromTheList", .5f);
+    }
+
+    void InitTroubleShootUI(int troubleShootingIndex, Transform panelParent)
+    {
+
+        //If current loop symbol is equal to prev image then dont instantiate
+        textIsUpdated = false;
+        if (carTroubleShootings[troubleShootingIndex].getSpriteSymbol != prevImage)
+        {
+            troubleShootUI = Instantiate(troubleShootPrefab, panelParent.position, Quaternion.identity);
+            troubleShootUI.transform.parent = panelParent;
+
+            Image symbol = troubleShootUI.transform.Find("CarSymbol_Img").GetComponent<Image>();
+            prevImage = carTroubleShootings[troubleShootingIndex].getSpriteSymbol;
+            symbol.sprite = carTroubleShootings[troubleShootingIndex].getSpriteSymbol;
+        }
+        else
+        {
+            TextMeshProUGUI textMesh = troubleShootUI.GetComponentInChildren<TextMeshProUGUI>();
+            prevText = textMesh;
+            prevText.text += $"\n {SetInstructionTxt(carTroubleShootings[troubleShootingIndex].GetSmokeColorEnum, carTroubleShootings[troubleShootingIndex].GetKeyCodes, carTroubleShootings[troubleShootingIndex].timeToHold)}";
+            textIsUpdated = true;
+        }
+
+        if (!textIsUpdated)
+        {
+            TextMeshProUGUI textMesh = troubleShootUI.GetComponentInChildren<TextMeshProUGUI>();
+            textMesh.text = SetInstructionTxt(carTroubleShootings[troubleShootingIndex].GetSmokeColorEnum, carTroubleShootings[troubleShootingIndex].GetKeyCodes, carTroubleShootings[troubleShootingIndex].timeToHold);
+            prevText = textMesh;
+        }
+    }
+
+    private void RemoveAllEmptyObjectsFromTheList()
+    {
+        for (int i = troubleShootPanels.Count - 1; i >= 0; i--)
+        {
+            if (troubleShootPanels[i] == null)
+            {
+                troubleShootPanels.RemoveAt(i);
             }
         }
     }
--- Delivery/Assets/Scripts/Navigator/VoiceCallManager.cs
+++ Delivery/Assets/Scripts/Navigator/VoiceCallManager.cs
@@ -13,13 +13,16 @@ public class VoiceCallManager : MonoBehaviour
 
 
     [Header("=== DIALOUGES ===")]
+    [SerializeField] float writingSpeed = .05f;
     [SerializeField] float disableTime = 5f;
     [SerializeField] string[] welcomeMessages;
 
     public void EnableWelcomeCall()
     {
         int index = Random.Range(0, welcomeMessages.Length);
-        bubbleMsg.text = welcomeMessages[index];
+
+        StartCoroutine(WritingEffect(welcomeMessages[index].ToCharArray()));
+        
         bubbleUI.SetActive(true);
         voiceCallUIAnim.SetBool("ShakeIcon", true);
         StartCoroutine(DisableWelcomeCall());
@@ -30,5 +33,17 @@ public class VoiceCallManager : MonoBehaviour
         yield return new WaitForSeconds(disableTime);
         bubbleUI.SetActive(false);
         voiceCallUIAnim.SetBool("ShakeIcon", false);
+    }
+
+    IEnumerator WritingEffect(char[] characters)
+    {
+        bubbleMsg.text = "";
+        yield return null;
+
+        for (int i = 0; i < characters.Length; i++)
+        {
+            yield return new WaitForSeconds(writingSpeed);
+            bubbleMsg.text += characters[i];
+        }
     }
 }
